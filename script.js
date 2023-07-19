8406f978c2e7d3e1b624facccda9b92fcdfb76ed
syfanadya <syfanadya5@gmail.com>
2023-07-20 03:03:24 +0700

    PUSH

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Student in Telkom University", "Informatics Engineering" ],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop: true
+
+})
